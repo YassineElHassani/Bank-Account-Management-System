1e032b2768328999603a2a4bf1c07960d6001644
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-28 19:20:49 +0100

    finishing the class with getter&setter

--- classes/savingsAccount.php
+++ classes/savingsAccount.php
@@ -1,6 +1,23 @@
 <?php
-require_once '../config/db_conn.php';
+require_once 'account.php';
 
+class savingsAccount extends account {
 
+    private $interest;
+
+    public function __construct($customerName, $balance, $interest) {
+        parent::__construct($customerName, $balance);
+        $this->interest = $interest;
+    }
+
+    public function getInterest() {
+        return $this->interest;
+    }
+
+    public function setInterest($newInterest) {
+        $this->interest = $newInterest;
+    }
+
+}
 
 ?>
