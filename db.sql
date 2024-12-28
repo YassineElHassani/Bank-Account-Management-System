4dd9d0014bc4067942f0ac386da6bee6097e3065
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-28 19:26:29 +0100

    sql code for creation the DB

--- db.sql
+++ db.sql
@@ -4,23 +4,23 @@ USE bank;
 CREATE TABLE account(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     customerName VARCHAR(100) NOT NULL,
-    balance FLOAT
+    balance FLOAT NOT NULL,
 );
 
 CREATE TABLE SavingsAccount(
-	accountID INT PRIMARY KEY AUTO_INCREMENT,
+	accountID INT NOT NULL,
     interest FLOAT,
     FOREIGN KEY (accountID) REFERENCES account(id)
 );
 
 CREATE TABLE currentAccount(
-	accountID INT PRIMARY KEY AUTO_INCREMENT,
+	accountID INT NOT NULL,
     overdraftLimit FLOAT,
     FOREIGN KEY (accountID) REFERENCES account(id)
 );
 
 CREATE TABLE businessAccount(
-	accountID INT PRIMARY KEY AUTO_INCREMENT,
+	accountID INT NOT NULL,
     transactionFee FLOAT,
     FOREIGN KEY (accountID) REFERENCES account(id)
 );
