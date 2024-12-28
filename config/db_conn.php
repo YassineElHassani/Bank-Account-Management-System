e74c66ba6847669e5331822f1b9f95ac4f2383b1
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-28 19:21:53 +0100

    adding define connection

--- config/db_conn.php
+++ config/db_conn.php
@@ -4,12 +4,12 @@ $userName = "root";
 $passWord = "";
 $dataBase = "bank";
 
-
 try {
     $dns = "mysql:host=".$serverName. ";dbname=".$dataBase;
     $pdo = new PDO($dns,$userName, $passWord);
-    // session_start();
-    // defined("pdo","$pdo");
+
+    define('pdo', $pdo);
+
 } catch(PDOException) {
     echo "Connection failed";
 }
