a74bd0612f53973f857fe70ebb6e85d859ef9494
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-30 09:58:10 +0100

    displaying messages for all actions

--- index.php
+++ index.php
@@ -4,11 +4,38 @@ require_once "./classes/accountManger.php";
 $manager = new AccountManager();
 $account = $manager->displayData();
 
+session_start();
+if (isset($_SESSION['edit_msg']) && $_SESSION['edit_msg'] === true) {
+    $editMessage = "Account updated successfully!";
+    unset($_SESSION['edit_msg']);
+}
+
+if (isset($_SESSION['delete_msg']) && $_SESSION['delete_msg'] === true) {
+    $deleteMessage = "Account deleted successfully!";
+    unset($_SESSION['delete_msg']);
+}
+
+
 ?>
 
 <?php
 include './layout/header.php'
 ?>
+
+<?php if (!empty($editMessage)): ?>
+    <div class="bg-green-100 text-green-700 p-3 rounded mb-5">
+        <?= htmlspecialchars($editMessage) ?>
+    </div>
+<?php endif; ?>
+
+<?php if (!empty($deleteMessage)): ?>
+    <div class="bg-green-100 text-green-700 p-3 rounded mb-5">
+        <?= htmlspecialchars($deleteMessage) ?>
+    </div>
+<?php endif; ?>
+
+<!-- Accounts Table -->
+<div class="bg-white rounded-lg shadow overflow-hidden">
 
 <table class="min-w-full">
     <thead class="bg-gray-50">
@@ -21,7 +48,7 @@ include './layout/header.php'
         </tr>
     </thead>
     <tbody class="divide-y divide-gray-200">
-    <?php 
+        <?php
         $i = 0;
         while ($i < count($account)): ?>
             <tr class="hover:bg-gray-50">
@@ -30,12 +57,17 @@ include './layout/header.php'
                 <td class="px-6 py-4"><?php echo htmlspecialchars($account[$i]['accountType']) ?></td>
                 <td class="px-6 py-4"><?php echo htmlspecialchars($account[$i]['balance']) ?></td>
                 <td class="px-6 py-4">
-                    <button class="focus:outline-none bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><img src="./assest/editing.png" alt="edit" height="20px" width="20px"></button>
-                    <button class="focus:outline-none bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><img src="./assest/delete.png" alt="delete" height="20px" width="20px"></button>
+                    <a href="editBankAccount.php?id=<?php echo htmlspecialchars($account[$i]['id']); ?>">
+                        <button class=" focus:outline-none bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><img src="./assets/editing.png" alt="edit" height="20px" width="20px"></button>
+                    </a>
+
+                    <a href="deleteBankAccount.php?id=<?php echo htmlspecialchars($account[$i]['id']); ?>">
+                        <button class="focus:outline-none bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><img src="./assets/delete.png" alt="delete" height="20px" width="20px"></button>
+                    </a>
                 </td>
             </tr>
-        <?php 
-        $i++;
+        <?php
+            $i++;
         endwhile; ?>
     </tbody>
 </table>
