a769ee7022e43962723b41278ba7e6624a8ca6ad
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-30 10:19:15 +0100

    changing the edit & delete functions

--- classes/accountManger.php
+++ classes/accountManger.php
@@ -61,49 +61,59 @@ class AccountManager {
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function getAccountById($id) {
+        $stmt = $this->conn->prepare("
+            SELECT account.id, account.customerName, account.balance,
+                   CASE 
+                       WHEN businessAccount.accountID IS NOT NULL THEN 'Business Account'
+                       WHEN currentAccount.accountID IS NOT NULL THEN 'Current Account'
+                       WHEN savingsAccount.accountID IS NOT NULL THEN 'Savings Account'
+                   END AS accountType,
+                   COALESCE(businessAccount.transactionFee, currentAccount.overdraftLimit, savingsAccount.interest) AS specificDetail
+            FROM account
+            LEFT JOIN businessAccount ON account.id = businessAccount.accountID
+            LEFT JOIN currentAccount ON account.id = currentAccount.accountID
+            LEFT JOIN savingsAccount ON account.id = savingsAccount.accountID
+            WHERE account.id = :id
+        ");
+    
+        $stmt->execute([':id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
     
 
-    public function editData($id, Account $account) {
-        $stmt = $this->conn->prepare("UPDATE account 
-                                      SET customerName = :customerName, balance = :balance 
-                                      WHERE id = :id");
+    public function editData($id, $customerName, $balance, $accountType, $specificDetail) {
 
-        $stmt->execute([
-            ':customerName' => $account->getName(),
-            ':balance' => $account->getBalance(),
-            ':id' => $id
-        ]);
+        $stmt = $this->conn->prepare("UPDATE account SET customerName = :customerName, balance = :balance WHERE id = :id");
+        $stmt->execute([':customerName' => $customerName, ':balance' => $balance, ':id' => $id]);
 
-        if ($account instanceof SavingsAccount) {
-            $stmt = $this->conn->prepare("UPDATE savingsAccount 
-                                          SET interest = :interest 
-                                          WHERE accountID = :account_id");
-            $stmt->execute([
-                ':interest' => $account->getInterest(),
-                ':account_id' => $id
-            ]);
-        } elseif ($account instanceof CurrentAccount) {
-            $stmt = $this->conn->prepare("UPDATE currentAccount 
-                                          SET overdraftLimit = :overdraftLimit 
-                                          WHERE accountID = :account_id");
-            $stmt->execute([
-                ':overdraftLimit' => $account->getOverdraftLimit(),
-                ':account_id' => $id
-            ]);
-        } elseif ($account instanceof BusinessAccount) {
-            $stmt = $this->conn->prepare("UPDATE businessAccount 
-                                          SET transactionFee = :transactionFee 
-                                          WHERE accountID = :account_id");
-            $stmt->execute([
-                ':transactionFee' => $account->getTransactionFee(),
-                ':account_id' => $id
-            ]);
+        if ($accountType === 'Savings') {
+            $stmt = $this->conn->prepare("UPDATE savingsAccount SET interest = :specificDetail WHERE accountID = :id");
+        } elseif ($accountType === 'Current') {
+            $stmt = $this->conn->prepare("UPDATE currentAccount SET overdraftLimit = :specificDetail WHERE accountID = :id");
+        } elseif ($accountType === 'Business') {
+            $stmt = $this->conn->prepare("UPDATE businessAccount SET transactionFee = :specificDetail WHERE accountID = :id");
         }
+        $stmt->execute([':specificDetail' => $specificDetail, ':id' => $id]);
     }
+    
 
     public function deleteData($id) {
+
+        $stmt = $this->conn->prepare("DELETE FROM savingsAccount WHERE accountID = :id");
+        $stmt->execute([':id' => $id]);
+
+        $stmt = $this->conn->prepare("DELETE FROM currentAccount WHERE accountID = :id");
+        $stmt->execute([':id' => $id]);
+
+        $stmt = $this->conn->prepare("DELETE FROM businessAccount WHERE accountID = :id");
+        $stmt->execute([':id' => $id]);
+
         $stmt = $this->conn->prepare("DELETE FROM account WHERE id = :id");
         $stmt->execute([':id' => $id]);
+
     }
 }
 
